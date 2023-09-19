8bce017ba9b3b0ef63c336c1009337769d42a568
Nweke Chidi <nwekechidigodwin460@gmail.com>
2023-09-19 17:53:44 +0100

    first commit Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 // src/App.tsx
-import React from 'react';
-import TableComponent from './components/TableComponent';
-import { countries } from './data/countries'; // Assuming you have a file with country data
+import React from "react";
+import TableComponent from "./components/TableComponent";
+import { countries } from "./data/countries"; // Assuming you have a file with country data
 
 const App: React.FC = () => {
   return (
-    <div >
-      <h1 style={{ padding: '20px' }}>The Country</h1>
+    <div>
+      <h1 style={{ padding: "20px" }}>The Country</h1>
       <TableComponent countries={countries} />
     </div>
   );
